72068a3db3d9e4773d8ff54197da27bd9a0cb76d
andrey <andreyoliveira.dev@gmail.com>
2025-01-29 08:48:51 -0300

    ordem correta de execução no script

--- script.sql
+++ script.sql
@@ -1,3 +1,12 @@
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    tipo INT NOT NULL DEFAULT 1
+);
+
+
 CREATE TABLE produtos (
     id_produto INT AUTO_INCREMENT PRIMARY KEY,
     descricao VARCHAR(255) NOT NULL,
@@ -8,10 +17,3 @@ CREATE TABLE produtos (
     FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
 
-CREATE TABLE usuario (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    tipo INT NOT NULL DEFAULT 1
-);
